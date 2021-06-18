4486ac48910c2bb5982a5eceadfd1860d3b9f751
Talha Salman <talha.salman22@gmail.com>
2021-06-18 21:50:05 +0500

    Update add_view.dart
    Created the Add Coins functionality.

--- lib/ui/add_view.dart
+++ lib/ui/add_view.dart
@@ -8,11 +8,63 @@ class AddView extends StatefulWidget {
 }
 
 class _AddViewState extends State<AddView> {
+  List<String> coins = [
+    'bitcoin',
+    'tether',
+    'etherium',
+  ];
+  String? dropdownValue = 'bitcoin';
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Hello"),
+      child: Column(
+        children: [
+          DropdownButton(
+            value: dropdownValue,
+            onChanged: (String? value) {
+              setState(() {
+                dropdownValue = value;
+              });
+            },
+            items: coins.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 1.3,
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Coin Amount',
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 1.4,
+            height: 45.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),
+              color: Colors.white,
+            ),
+            child: MaterialButton(
+              onPressed: () async {
+                // TODO: Add Button
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
