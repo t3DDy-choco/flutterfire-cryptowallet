d7bf72a88762bb798ecf4b28717b28025b6f1ee1
Talha Salman <talha.salman22@gmail.com>
2021-06-18 19:26:46 +0500

    Update flutterfire.dart
    Added Register and Sign In functions.

---
+++ lib/net/flutterfire.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<bool> register(String email, String password) async {
+  try {
+    FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return true;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('Email is already in use.');
+    }
+    return false;
+  } catch (e) {
+    print(e);
+    return false;
+  }
+}
+
+Future<bool> signIn(String email, String password) async {
+  try {
+    FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+    return true;
+  } catch (e) {
+    print(e);
+    return false;
+  }
+}
