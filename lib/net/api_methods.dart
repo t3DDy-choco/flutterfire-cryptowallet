377c99a0ff9439ed628c81ad16ff56995a04639d
Talha Salman <talha.salman22@gmail.com>
2021-06-18 23:36:08 +0500

    Update api_methods.dart
    Added api method to retrieve value of coin in usd.

--- lib/net/api_methods.dart
+++ lib/net/api_methods.dart
@@ -1,1 +1,15 @@
-// https://api.coingecko.com/api/v3/coins/
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<double> getPrice(String id) async {
+  try {
+    var url = Uri.parse("https://api.coingecko.com/api/v3/coins/" + id);
+    var response = await http.get(url);
+    var json = jsonDecode(response.body);
+    var value = json['market_data']['current_price']['usd'].toString();
+    return double.parse(value);
+  } catch (e) {
+    print(e);
+  }
+  return 0.0;
+}
