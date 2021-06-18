b4ba53b1529d59fb4c9286cb1b8943f597ec381c
Talha Salman <talha.salman22@gmail.com>
2021-06-18 20:19:26 +0500

    Update home_view.dart
    Added the View for the Coins details.

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -11,7 +13,48 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello"),
+      body: Container(
+        child: Center(
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('Users')
+                .doc(FirebaseAuth.instance.currentUser!.uid)
+                .collection('Coins')
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData)
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              return ListView(
+                children: snapshot.data!.docs.map((document) {
+                  return Container(
+                    child: Row(
+                      children: [
+                        Text("Coin Name: ${document.id}"),
+                        Text(
+                            "Amount Owned: ${(document.data() as dynamic)['Amount']}"),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              );
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.push(
+          //     context, MaterialPageRoute(builder: (context) => AddView()));
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.blueAccent,
+      ),
     );
   }
 }
